e3f7515bfaed4b96d52271bd0ad2409d29deae8e
Yuta Matsuo <y.matsuo.dev@gmail.com>
2024-03-02 14:13:23 +0900

    feat: add icon color red

--- src/components/ui/Icon/type.ts
+++ src/components/ui/Icon/type.ts
@@ -1,4 +1,4 @@
 import type { ICON_NAMES } from '@/components/ui/Icon/const';
 
 export type IconName = (typeof ICON_NAMES)[number];
-export type IconColorType = 'main' | 'sub' | 'dark' | 'light';
+export type IconColorType = 'main' | 'sub' | 'dark' | 'light' | 'red';
