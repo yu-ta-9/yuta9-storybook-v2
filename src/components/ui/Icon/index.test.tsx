30b4bf983d000fb30b6c8f7e1659de9aaa717dcf
Yuta Matsuo <y.matsuo.dev@gmail.com>
2024-04-22 08:48:11 +0900

    fix: test

--- src/components/ui/Icon/index.test.tsx
+++ src/components/ui/Icon/index.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 
 import { Icon } from '@/components/ui/Icon';
+import { ICON_NAMES } from '@/components/ui/Icon/const';
 
 describe('Icon', () => {
-  test('area-hidden="true"が設定されていること', () => {
-    render(<Icon name='asterisk' size={24} />);
-    const icon = screen.getByTestId('asterisk');
+  test.each(ICON_NAMES)('area-hidden="true"が設定されていること', (name) => {
+    render(<Icon name={name} size={24} />);
+    const icon = screen.getByTestId(name);
     expect(icon).toHaveAttribute('aria-hidden', 'true');
   });
 });
