6a24d58f95b6d8ec573e13da07f2d68222bafb11
Yuta Matsuo <y.matsuo.dev@gmail.com>
2024-11-24 11:52:52 +0900

    feat: add Accordion

---
+++ src/components/ui/Accordion/index.tsx
@@ -0,0 +1,39 @@
+import { type ReactElement, useId, useState, type FC } from 'react';
+
+import styles from '@/components/ui/Accordion/index.module.css';
+import clsx from 'clsx';
+
+type Props = {
+  label: string;
+  innerElement: ReactElement;
+};
+
+export const Accordion: FC<Props> = ({ label, innerElement }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const buttonId = useId();
+  const panelId = useId();
+
+  return (
+    <div className={styles.accordion}>
+      <button
+        className={styles.button}
+        id={buttonId}
+        aria-expanded={isOpen}
+        aria-controls={panelId}
+        type='button'
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {label}
+      </button>
+
+      <div
+        id={panelId}
+        aria-labelledby={buttonId}
+        role='region'
+        className={clsx(styles.inner, isOpen ? '' : styles.hide)}
+      >
+        {innerElement}
+      </div>
+    </div>
+  );
+};
